a277f7fa2e799816bde767a147133161148d0db4
DaemonCantor <DaemonCantor@gmaers.webhop.net>
2011-01-30 10:28:36 -0500

    unit.h
    acopy of a correct unit.h from Vladex's repo

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1486,6 +1486,8 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         void SendMonsterMove(float x, float y, float z, SplineType type, SplineFlags flags, uint32 Time, Player* player = NULL, ...);
         void SendMonsterMoveWithSpeed(float x, float y, float z, uint32 transitTime = 0, Player* player = NULL);
         void SendMonsterMoveTransport(WorldObject *transport, SplineType type, SplineFlags flags, uint32 moveTime, ...);
+		template<typename PathElem, typename PathNode>
+		void SendMonsterMoveByPath(Path<PathElem,PathNode> const& path, uint32 start, uint32 end, SplineFlags flags);
 
         virtual bool SetPosition(float x, float y, float z, float orientation, bool teleport = false);
 
@@ -2202,4 +2204,5 @@ inline void Unit::SendMonsterMoveByPath(Path<Elem,Node> const& path, uint32 star
 }
 
 #endif
+
 
